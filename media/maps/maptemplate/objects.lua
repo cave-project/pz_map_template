2ce28e4b31ee43e07315c639cdd84d3306819f77
p4kz <wolpak@hotmail.com>
2023-02-07 12:10:17 -0300

    create map template

---
+++ media/maps/maptemplate/objects.lua
@@ -0,0 +1,3 @@
+objects = {
+  { name = "", type = "SpawnPoint", x = 240, y = 135, z = 0, width = 1, height = 1, properties = { Professions = "all" } }
+}
---
+++ media/maps/maptemplate/spawnpoints.lua
@@ -0,0 +1,22 @@
+function SpawnPoints()
+return {
+  constructionworker = {
+    { worldX = 0, worldY = 0, posX = 240, posY = 135, posZ = 0 }
+  },
+  fireofficer = {
+    { worldX = 0, worldY = 0, posX = 240, posY = 135, posZ = 0 }
+  },
+  parkranger = {
+    { worldX = 0, worldY = 0, posX = 240, posY = 135, posZ = 0 }
+  },
+  policeofficer = {
+    { worldX = 0, worldY = 0, posX = 240, posY = 135, posZ = 0 }
+  },
+  securityguard = {
+    { worldX = 0, worldY = 0, posX = 240, posY = 135, posZ = 0 }
+  },
+  unemployed = {
+    { worldX = 0, worldY = 0, posX = 240, posY = 135, posZ = 0 }
+  }
+}
+end
